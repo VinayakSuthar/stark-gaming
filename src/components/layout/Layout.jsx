056506c5e0f5eeac34aafdde517274c8b32dc29c
vinayak <vinayak@gkmit.co>
2022-12-01 17:55:58 +0530

    Style tablet navigation bar

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavigationBar from '../navigation/NavigationBar'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavigationBar from '../navigation/NavigationBar';
+
+import "./Layout.css";
 
 export default function Layout() {
   return (
--- src/components/navigation/NavigationBar.jsx
+++ src/components/navigation/NavigationBar.jsx
@@ -1,35 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { BiHomeAlt, BiSearchAlt } from "react-icons/bi";
-import {
-  MdSlowMotionVideo,
-  MdOutlineFactCheck,
-} from "react-icons/md";
+import { MdSlowMotionVideo, MdOutlineFactCheck } from "react-icons/md";
 
 import "./NavigationBar.css";
 
 export default function NavigationBar() {
   const [activeIndex, setActiveIndex] = useState("home");
-  
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", resize);
+  },[]);
+
+  function resize() {
+    setInnerWidth(window.innerWidth);
+  }
+
   return (
     <div className="nav-bar">
-      <div className="logo">Stark Gaming</div>
+      <div className="logo">
+        {
+          innerWidth>992 ? "Stark Gaming": "SG"
+        }
+      </div>
       <div className="nav-menu">
         <NavLink to="/" className="nav-link">
           <BiHomeAlt size="1.1em" />
-          Home
+          <span>Home</span>
         </NavLink>
         <NavLink to="browse" className="nav-link">
           <BiSearchAlt />
-          Browse
+          <span>Browse</span>
         </NavLink>
         <Link className="nav-link">
           <MdSlowMotionVideo />
-          Video
+          <span>Video</span>
         </Link>
         <Link className="nav-link">
           <MdOutlineFactCheck />
-          Compatibility
+          <span>Compatibility</span>
         </Link>
       </div>
     </div>
