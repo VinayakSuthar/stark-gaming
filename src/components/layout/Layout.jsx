38dd454c751f143bec6be697415b508a91292205
vinayak <vinayak@gkmit.co>
2022-12-02 11:11:33 +0530

    Add top navigation bar

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import NavigationBar from '../navigation/NavigationBar';
-import {GiHamburgerMenu} from 'react-icons/gi';
+import React, { useEffect, useRef, useState } from "react";
+import { Outlet } from "react-router-dom";
 
 import "./Layout.css";
 
+import NavigationBar from "../navigation/NavigationBar";
+import TopNavigation from "../top-navigation/TopNavigation";
+
 export default function Layout() {
-  const [open, setOpen] = useState(window.innerWidth < 601 ? false: true);
+  const [open, setOpen] = useState(window.innerWidth < 601 ? false : true);
   const navRef = useRef(null);
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     const outsideClick = (event) => {
-      if(navRef.current && !navRef.current.contains(event.target)) {
+      if (navRef.current && !navRef.current.contains(event.target)) {
         setOpen(false);
       }
     };
-    document.addEventListener("click", outsideClick, true);
-    return ()=> {
-      document.removeEventListener("click", outsideClick, true);
+    if (window.innerWidth < 600) {
+      document.addEventListener("click", outsideClick, true);
+      return () => {
+        document.removeEventListener("click", outsideClick, true);
+      };
     }
-  },[])
-  
+  }, []);
+
   return (
     <div className="home-layout">
-      <GiHamburgerMenu className="mobile" size="1.4em" color="#e5414e" onClick={()=>setOpen(!open)}/>
-      {open && <NavigationBar navRef={navRef} /> }
+      <TopNavigation setOpen={setOpen} />
+      {open && <NavigationBar navRef={navRef} />}
       <Outlet />
     </div>
   );
