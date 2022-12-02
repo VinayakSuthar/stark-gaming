a5fd884bdcd7e98898d342b515b6dc5889582bf0
vinayak <vinayak@gkmit.co>
2022-12-02 10:15:57 +0530

    Style mobile nav bar

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import NavigationBar from '../navigation/NavigationBar';
+import {GiHamburgerMenu} from 'react-icons/gi';
 
 import "./Layout.css";
 
 export default function Layout() {
+  const [open, setOpen] = useState(window.innerWidth < 601 ? false: true);
+  const navRef = useRef(null);
+  
+  useEffect(()=> {
+    const outsideClick = (event) => {
+      if(navRef.current && !navRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("click", outsideClick, true);
+    return ()=> {
+      document.removeEventListener("click", outsideClick, true);
+    }
+  },[])
+  
   return (
-    <div className='home-layout'>
-      <NavigationBar />
+    <div className="home-layout">
+      <GiHamburgerMenu className="mobile" size="1.4em" color="#e5414e" onClick={()=>setOpen(!open)}/>
+      {open && <NavigationBar navRef={navRef} /> }
       <Outlet />
     </div>
-  )
+  );
 }
