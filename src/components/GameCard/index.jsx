7cebdc73b54f07de492a8800ed12f763902da3c4
vinayak <vinayak@gkmit.co>
2022-12-03 18:12:49 +0530

    Feature redirect to detail page on click

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -1,14 +1,29 @@
+import { Link } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 
 import "./index.css";
 
 export default function GameCard({ gameData }) {
-  const { background_image, name, genres } = gameData;
+  const { id,background_image, name, genres } = gameData;
   return (
     <div className="game-card">
-      {background_image ? <img src={background_image} alt="game" /> : <Skeleton height={141}/>}
-      {name ? <h3 className="game-title">{name}</h3> : <Skeleton width="80%"/>}
-      {genres ? <p className="game-genre">{genres[0].name}</p>: <Skeleton width="40%"/>}
+      {background_image ? (
+        <img src={background_image} alt="game" />
+      ) : (
+        <Skeleton height={141} />
+      )}
+      {name ? (
+        <Link to={`/browse/${id}`}>
+          <h3 className="game-title">{name}</h3>
+        </Link>
+      ) : (
+        <Skeleton width="80%" />
+      )}
+      {genres ? (
+        <p className="game-genre">{genres[0].name}</p>
+      ) : (
+        <Skeleton width="40%" />
+      )}
     </div>
   );
 }
