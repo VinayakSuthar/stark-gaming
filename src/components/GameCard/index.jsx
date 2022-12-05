d479bcbba243be119007e5596c20b39a55d3e432
vinayak <vinayak@gkmit.co>
2022-12-05 16:48:26 +0530

    Fix remove skeleton from game card

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Skeleton from "react-loading-skeleton";
 
 import "./index.css";
 
@@ -7,23 +6,11 @@ export default function GameCard({ gameData }) {
   const { id, background_image, name, genres } = gameData;
   return (
     <div className="game-card">
-      {background_image ? (
-        <img src={background_image} alt="game" />
-      ) : (
-        <Skeleton height={141} />
-      )}
-      {name ? (
-        <Link to={`/browse/${id}`}>
-          <h3 className="game-title">{name}</h3>
-        </Link>
-      ) : (
-        <Skeleton width="80%" />
-      )}
-      {genres ? (
-        <p className="game-genre">{genres[0].name}</p>
-      ) : (
-        <Skeleton width="40%" />
-      )}
+      <img src={background_image} alt="game" />
+      <Link to={`/browse/${id}`}>
+        <h3 className="game-title">{name}</h3>
+      </Link>
+      <p className="game-genre">{genres[0]?.name}</p>
     </div>
   );
 }
