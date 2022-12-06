0b65073a2a9faa73acba126614a380d79c7fddfd
vinayak <vinayak@gkmit.co>
2022-12-06 18:12:40 +0530

    Fix skeleton card reponsive

--- src/components/SkeletonCard/index.jsx
+++ src/components/SkeletonCard/index.jsx
@@ -1,9 +1,9 @@
 import Skeleton from "react-loading-skeleton";
 
-export default function SkeletonCard() {
+export default function SkeletonCard({ cardStyle }) {
   return (
-    <div style={{ width: "250px" }}>
-      <Skeleton height={141} />
+    <div className={cardStyle}>
+      <Skeleton style={{ aspectRatio: "266/150" }} />
       <Skeleton width="80%" />
       <Skeleton width="40%" />
     </div>
