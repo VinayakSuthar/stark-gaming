1998e872c8899d69c8aa1d80ed4c63f6fcc98737
vinayak <vinayak@gkmit.co>
2022-12-11 10:58:54 +0530

    Add game card test

---
+++ src/components/GameCard/__test__/index.test.jsx
@@ -0,0 +1,79 @@
+import { render, screen } from "@testing-library/react";
+import { vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+import useLocalStorage from "../../../hooks/useLocalStorage";
+import GameCard from "..";
+
+beforeEach(() => {
+  Object.defineProperty(window, "localStorage", {
+    value: {
+      getItem: vi.fn(() => null),
+      setItem: vi.fn(() => null),
+    },
+    writable: true,
+  });
+});
+
+const MockGameCard = () => {
+  const [wishlist, setWishlist] = useLocalStorage("wishlist", []);
+
+  function addGameToWishlist(id) {
+    if (wishlist.includes(id)) {
+      const newList = wishlist.filter((item) => item !== id);
+      setWishlist(newList);
+    } else {
+      setWishlist((previousList) => [...previousList, id]);
+    }
+  }
+
+  const gameData = {
+    id: 3489,
+    background_image:
+      "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
+    name: "GTA V",
+    genres: [{ name: "Action" }],
+  };
+  return (
+    <BrowserRouter>
+      <GameCard
+        gameData={gameData}
+        addGameToWishlist={addGameToWishlist}
+        wishlist={wishlist}
+      />
+    </BrowserRouter>
+  );
+};
+
+describe("Game card component", () => {
+  it("should render game card correctly", () => {
+    render(<MockGameCard />);
+    const image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
+  });
+
+  it("should have game name GTA V", () => {
+    render(<MockGameCard />);
+    const gameTitle = screen.getByRole("heading");
+    expect(gameTitle).toHaveTextContent("GTA V");
+  });
+
+  it("should have genre", () => {
+    render(<MockGameCard />);
+    const gameGenre = screen.getByText(/action/i);
+    expect(gameGenre).toBeInTheDocument();
+  });
+
+  it("should have 'Want to play' button", () => {
+    render(<MockGameCard />);
+    const wantToPlayButton = screen.getByRole("button");
+    expect(wantToPlayButton).toHaveTextContent(/want to play/i);
+  });
+
+  it("should able to click 'Want to play' button", () => {
+    render(<MockGameCard />);
+    const wantToPlayButton = screen.getByRole("button");
+    userEvent.click(wantToPlayButton);
+    expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
+  });
+});
