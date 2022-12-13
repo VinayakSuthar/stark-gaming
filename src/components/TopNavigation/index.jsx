e66479b9d9cca0f97463e63438eb8f29f5ed60b2
vinayak <vinayak@gkmit.co>
2022-12-13 18:13:08 +0530

    Add search bar

--- src/components/TopNavigation/index.jsx
+++ src/components/TopNavigation/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { AiFillSetting } from "react-icons/ai";
 import { FaRegUserCircle } from "react-icons/fa";
+import { BiSearch } from "react-icons/bi";
+
+import SearchBox from "../SearchBox";
 
 import "./index.css";
 
@@ -19,28 +21,27 @@ export default function TopNavigation({ setOpen }) {
   }
 
   return (
-    <div className="top-nav">
-      <GiHamburgerMenu
-        className="mobile"
-        size="1.8em"
-        color="#c7d5e0"
-        onClick={handleOpen}
-      />
+    <>
+      <div className="top-nav">
+        <GiHamburgerMenu
+          className="mobile"
+          size="1.8em"
+          color="#c7d5e0"
+          onClick={handleOpen}
+        />
 
-      <p className="mobile mobile-logo">SG</p>
+        <p className="mobile mobile-logo">SG</p>
 
-      <input
-        className="search-input"
-        type="text"
-        placeholder="Search here"
-        value={searchText}
-        onChange={handleSearch}
-      />
-      <div>
-        <Link to="profile">
-          <FaRegUserCircle className="icon" size="1.5em" />
-        </Link>
+        <button className="search-bar-button">
+          <BiSearch /> Search
+        </button>
+        <div>
+          <Link to="profile">
+            <FaRegUserCircle className="icon" size="1.5em" />
+          </Link>
+        </div>
       </div>
-    </div>
+      <SearchBox />
+    </>
   );
 }
