809873bbf75c7c7ddb50410c4adbaf480de3f88e
vinayak <vinayak@gkmit.co>
2022-12-14 16:22:31 +0530

    Add search functionality

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,10 +1,43 @@
 import { useEffect, useRef } from "react";
+import useAxios from "../../hooks/useAxios";
 import { BiSearch } from "react-icons/bi";
 
+import loader from "../../assets/image/loader.svg";
 import "./index.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function SearchBox({ onClose }) {
+  const [previousController, setPreviousController] = useState();
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
   const searchRef = useRef(null);
+  const fetchGames = useAxios();
+
+  console.log("search-box");
+  function handleSearch(event) {
+    previousController?.abort();
+    const searchString = event.target.value;
+    const controller = new AbortController();
+    if (searchString.length === 0) {
+      setSearchResult([]);
+    }
+    if (searchString.length > 2) {
+      setLoading(true);
+      fetchGames
+        .get("games", {
+          params: { search: searchString },
+          signal: controller.signal,
+        })
+        .then((response) => {
+          setSearchResult(response.data.results);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+    setPreviousController(controller);
+  }
 
   useEffect(() => {
     const handleKeyPress = (event) => {
@@ -22,15 +55,42 @@ export default function SearchBox({ onClose }) {
       document.removeEventListener("click", outsideClick, true);
     };
   }, []);
-
   return (
     <div className="search-box-container">
       <div className="search-box" ref={searchRef}>
         <div className="search-bar">
           <BiSearch />
-          <input className="search-input" type="text" placeholder="Search..." />
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search..."
+            onChange={handleSearch}
+          />
         </div>
-        <div className="search-result">No result to show</div>
+        <div className="search-result">
+          {loading && (
+            <div className="loader-container">
+              <img className="loader-svg" src={loader} alt="loader" />
+            </div>
+          )}
+          {!loading &&
+            (searchResult.length === 0 ? (
+              "No result to show"
+            ) : (
+              <>
+                {searchResult.map(({ id, name, genres }) => {
+                  return (
+                    <Link to={`/browse/${id}`} key={id} onClick={onClose}>
+                      {name} <br />
+                      <span className="search-genre">
+                        {genres[0]?.name || "No data"}
+                      </span>
+                    </Link>
+                  );
+                })}
+              </>
+            ))}
+        </div>
       </div>
     </div>
   );
