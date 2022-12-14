96bda6b4a5987780a37c01349a797e2955301cc4
vinayak <vinayak@gkmit.co>
2022-12-14 10:43:37 +0530

    Fix status button test

--- src/components/StatusButton/__test__/index.test.jsx
+++ src/components/StatusButton/__test__/index.test.jsx
@@ -2,18 +2,28 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import StatusButton from "..";
 
 const MockStatusButton = () => {
-  return <StatusButton id="3456" />;
+  return (
+    <StatusButton
+      gameData={{
+        gameId: 1234,
+        name: "GTA V",
+        background_image: "any",
+        genres: [{ name: "Action" }],
+      }}
+    />
+  );
 };
 
 describe("Status button component", () => {
   it("should render status button correctly", () => {
-    render(<StatusButton />);
+    render(<MockStatusButton />);
     const statusButton = screen.getByTestId("active-status");
     expect(statusButton).toBeInTheDocument();
   });
 
   it("on hover show status list", () => {
-    render(<StatusButton />);
+    render(<MockStatusButton />);
+
     const dropdown = screen.getByTestId("dropdown");
     const dropdownContent = screen.getByTestId("dropdown-content");
     fireEvent.mouseOver(dropdown);
@@ -21,19 +31,22 @@ describe("Status button component", () => {
   });
 
   it("should have four buttons", () => {
-    render(<StatusButton />);
+    render(<MockStatusButton />);
+
     const buttonList = screen.getAllByRole("button");
     expect(buttonList).toHaveLength(4);
   });
 
   it("should not have check icon", () => {
-    render(<StatusButton />);
+    render(<MockStatusButton />);
+
     const checkIcon = screen.queryByTestId("check-icon");
     expect(checkIcon).not.toBeInTheDocument();
   });
 
   it("on click should show check icon", () => {
-    render(<StatusButton />);
+    render(<MockStatusButton />);
+
     const statusButton = screen.getByTestId("active-status");
     fireEvent.click(statusButton);
     const checkIcon = screen.queryByTestId("check-icon");
