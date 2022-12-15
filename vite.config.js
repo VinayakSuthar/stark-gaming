c1dcb426ba9d28d9ba857f090b7aca24b46dae57
vinayak <vinayak@gkmit.co>
2022-12-15 10:19:57 +0530

    Add absolute path config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./setupTest.js",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
