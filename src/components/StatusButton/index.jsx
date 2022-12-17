28445d6c9371c2fb33212672ba0da828c648ffba
vinayak <vinayak@gkmit.co>
2022-12-17 16:34:18 +0530

    Animate status button

--- src/components/StatusButton/index.jsx
+++ src/components/StatusButton/index.jsx
@@ -1,10 +1,54 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { FiCheck } from "react-icons/fi";
 
 import "./index.css";
+
+const dropdownAnimate = {
+  enter: {
+    opacity: 1,
+    clipPath: "inset(0% 0% 0% 0% round 10px)",
+    transition: {
+      type: "spring",
+      bounce: 0,
+      duration: 0.7,
+      delayChildren: 0.3,
+      staggerChildren: 0.05,
+    },
+    display: "block",
+  },
+  exit: {
+    opacity: 0,
+    clipPath: "inset(10% 50% 90% 50% round 10px)",
+    transition: {
+      type: "spring",
+      bounce: 0,
+      duration: 0.5,
+    },
+    transitionEnd: {
+      display: "none",
+    },
+  },
+};
+
+const statusAnimate = {
+  enter: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+  exit: { opacity: 0, x: 20, transition: { duration: 0.2 } },
+};
+
+const arrowAnimate = {
+  enter: { rotate: 180 },
+  exit: { rotate: 0 },
+};
+
+const MotionArrow = motion(RiArrowDropDownLine);
 
 export default function StatusButton({ gameData }) {
   const { gameId, name, background_image, genres } = gameData;
@@ -13,6 +57,7 @@ export default function StatusButton({ gameData }) {
   const [played, setPlayed] = useLocalStorage("played", []);
   const [playing, setPlaying] = useLocalStorage("playing", []);
   const [showCheck, setShowCheck] = useState(true);
+  const [isHover, setHover] = useState(false);
   const [activeBtn, setActiveBtn] = useState(() => {
     if (played.find((game) => game.id === intId)) {
       return "Played";
@@ -26,6 +71,10 @@ export default function StatusButton({ gameData }) {
     setShowCheck(false);
     return "Want to Play";
   });
+
+  function toggleHover() {
+    setHover((previous) => !previous);
+  }
 
   function handleActiveBtnClick(value) {
     if (value === "Want to Play") {
@@ -82,6 +131,7 @@ export default function StatusButton({ gameData }) {
   return (
     <div className="game-status-btn">
       {showCheck && <FiCheck data-testid="check-icon" />}
+
       <button
         data-testid="active-status"
         onClick={() => handleActiveBtnClick(activeBtn)}
@@ -89,14 +139,37 @@ export default function StatusButton({ gameData }) {
         {activeBtn}
       </button>
       <div data-testid="dropdown" className="dropdown-icon">
-        <div className="dropdown-icon">
-          <RiArrowDropDownLine />
-          <div data-testid="dropdown-content" className="dropdown-content">
-            <button onClick={handleStatus}>Want to Play</button>
-            <button onClick={handleStatus}>Playing</button>
-            <button onClick={handleStatus}>Played</button>
-          </div>
-        </div>
+        <motion.div
+          className="dropdown-icon"
+          onHoverStart={toggleHover}
+          onHoverEnd={toggleHover}
+        >
+          <motion.div
+            variants={arrowAnimate}
+            animate={isHover ? "enter" : "exit"}
+            transition={{ duration: 0.2 }}
+            style={{ originY: 0.5 }}
+          >
+            <MotionArrow />
+          </motion.div>
+          <motion.div
+            data-testid="dropdown-content"
+            className="dropdown-content"
+            initial="exit"
+            variants={dropdownAnimate}
+            animate={isHover ? "enter" : "exit"}
+          >
+            <motion.button variants={statusAnimate} onClick={handleStatus}>
+              Want to Play
+            </motion.button>
+            <motion.button variants={statusAnimate} onClick={handleStatus}>
+              Playing
+            </motion.button>
+            <motion.button variants={statusAnimate} onClick={handleStatus}>
+              Played
+            </motion.button>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
