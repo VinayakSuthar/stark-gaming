534b669e02d0c47c3e4601cc77a202c7bccf0bec
vinayak <vinayak@gkmit.co>
2022-12-18 10:17:46 +0530

    Fix search frequent api call
    implement debouncing
    create useDebounce hook

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useEffect } from "react";
+
+export default function useDebounce(effect, dependency, delay = 500) {
+  useEffect(() => {
+    const timeoutId = setTimeout(effect, delay);
+    return () => clearTimeout(timeoutId);
+  }, [dependency]);
+}
