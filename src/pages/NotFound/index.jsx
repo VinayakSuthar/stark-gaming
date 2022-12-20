bd4d300ec1f9e4584ec1af26835eaa16f23f1692
vinayak <vinayak@gkmit.co>
2022-12-20 10:06:41 +0530

    Style new 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import bugGif from "../../assets/image/bug.gif";
+
+import "./index.css";
 
 export default function NotFound() {
   return (
+    <div className="not-found">
       <h1>404 Page Not Found</h1>
+      <p>Congrats, you just found the bongo moth!</p>
+      <img src={bugGif} alt="bug gif" />
+      <Link to="/">Go To Home</Link>
+    </div>
   );
 }
