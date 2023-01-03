0528f56fe00e5ce6b96b450c9fbbfc741dff1ccd
vinayak <vinayak@gkmit.co>
2023-01-03 18:15:19 +0530

    Integrate strapi on game detail

--- src/components/GameDetail/index.jsx
+++ src/components/GameDetail/index.jsx
@@ -1,43 +1,55 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
 import parse from "html-react-parser";
 import GameDetailSkeleton from "../GameDetailSkeleton";
+
 import useAxios from "../../hooks/useAxios";
 import StatusButton from "../StatusButton";
 
 import "react-loading-skeleton/dist/skeleton.css";
 import "./index.css";
 
+const client = useAxios();
+function fetchGame({ queryKey }) {
+  const id = queryKey[1];
+  return client.get(`games/${id}`, {
+    params: {
+      populate: "*",
+    },
+  });
+}
+
 export default function GameDetail() {
   const { id: gameId } = useParams();
-  const [gameData, setGameData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const fetchGames = useAxios();
+
+  const { data, isError, isLoading, error } = useQuery(
+    ["game", gameId],
+    fetchGame,
+    {
+      select: (data) => data.data.data,
+    }
+  );
+
   const {
-    name,
+    Name: name,
     background_image,
     description,
     genres,
-    developers,
-    released,
-    publishers,
+    Developer: developer,
+    Released: released,
+    Publisher: publisher,
     website,
-  } = gameData;
+  } = data?.attributes || {};
 
-  useEffect(() => {
-    setLoading(true);
-    fetchGames(`games/${gameId}`)
-      .then((response) => {
-        setGameData(response.data);
-      })
-      .catch((err) => {
-        setError(err);
-      })
-      .finally(() => setLoading(false));
-  }, [gameId]);
+  const sanitizedGenres = genres?.data?.map((genre) => {
+    const { name, slug } = genre.attributes;
+    return { id: genre.id, name, slug };
+  });
 
-  if (error) {
+  const sanitizedBackgroundImage = `http://localhost:1337${background_image?.data?.attributes?.url}`;
+
+  if (isError) {
+    console.log(error);
     if (error?.response.status === 404) {
       return <h1>Game Not Found</h1>;
     } else {
@@ -46,47 +58,46 @@ export default function GameDetail() {
   }
   return (
     <div className="game-detail">
-      {loading ? (
+      {isLoading ? (
         <GameDetailSkeleton />
       ) : (
         <>
           <h1 className="game-title">{name || <Skeleton width="400px" />}</h1>
           <div className="game-detail-container">
             <div className="game-content">
-              <img className="game-image" src={background_image} alt="game" />
+              <img
+                className="game-image"
+                src={sanitizedBackgroundImage}
+                alt="game"
+              />
 
               <div className="game-info">
                 <div>
                   <p className="subtitle">Genre: </p>
                   <p className="subtitle-data">
-                    {genres?.map(({ name }) => name).join(", ") || `No Data`}
+                    {sanitizedGenres?.map(({ name }) => name).join(", ") ||
+                      `No Data`}
                   </p>
                 </div>
                 <div>
                   <p className="subtitle">Developers: </p>
-                  <p className="subtitle-data">
-                    {developers?.map(({ name }) => name).join(", ") ||
-                      `No Data`}
-                  </p>
+                  <p className="subtitle-data">{developer || `No Data`}</p>
                 </div>
                 <div>
                   <p className="subtitle">Publisher: </p>
-                  <p className="subtitle-data">
-                    {publishers?.map(({ name }) => name).join(", ") ||
-                      `No Data`}
-                  </p>
+                  <p className="subtitle-data">{publisher || `No Data`}</p>
                 </div>
                 <div>
                   <p className="subtitle">Released Date: </p>
                   <p className="subtitle-data">{released || `No Data`}</p>
                 </div>
-                <div>
-                  <a className="game-site-link" target="_blank" href={website}>
-                    Visit Site
-                  </a>
-                </div>
                 <StatusButton
-                  gameData={{ gameId, name, background_image, genres }}
+                  gameData={{
+                    gameId,
+                    name,
+                    background_image: sanitizedBackgroundImage,
+                    genres,
+                  }}
                 />
               </div>
             </div>
