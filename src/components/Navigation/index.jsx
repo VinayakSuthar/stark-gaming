dfc075da9eea6a88e95d200968424bb3655e6c8e
vinayak <vinayak@gkmit.co>
2023-01-04 15:42:03 +0530

    Remove extra nav and add profile nav

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { BiHomeAlt, BiSearchAlt } from "react-icons/bi";
-import { MdSlowMotionVideo, MdOutlineFactCheck } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
 
 import "./index.css";
 
-export default function NavigationBar({navRef}) {
+export default function NavigationBar({ navRef }) {
   const [activeIndex, setActiveIndex] = useState("home");
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  console.log(
+    "🔥 ~ file: index.jsx:12 ~ NavigationBar ~ innerWidth",
+    innerWidth
+  );
 
   useEffect(() => {
     window.addEventListener("resize", resize);
-  },[]);
+  }, []);
 
   function resize() {
     setInnerWidth(window.innerWidth);
@@ -20,27 +24,25 @@ export default function NavigationBar({navRef}) {
   return (
     <div ref={navRef} className="nav-bar">
       <div className="logo desktop">
-        {
-          innerWidth>992 ? "Stark Gaming": "SG"
-        }
+        {innerWidth > 992 ? "Stark Gaming" : "SG"}
       </div>
       <div className="nav-menu">
-        <NavLink to="/" className="nav-link">
-          <BiHomeAlt size="1.1em" />
-          <span>Home</span>
-        </NavLink>
-        <NavLink to="browse" className="nav-link">
-          <BiSearchAlt />
-          <span>Browse</span>
-        </NavLink>
-        <Link className="nav-link">
-          <MdSlowMotionVideo />
-          <span>Video</span>
-        </Link>
-        <Link className="nav-link">
-          <MdOutlineFactCheck />
-          <span>Compatibility</span>
-        </Link>
+        <div className="top-menu">
+          <NavLink to="/" className="nav-link">
+            <BiHomeAlt size="1.1em" />
+            <span>Home</span>
+          </NavLink>
+          <NavLink to="browse" className="nav-link">
+            <BiSearchAlt />
+            <span>Browse</span>
+          </NavLink>
+        </div>
+        <div className="bottom-menu">
+          <NavLink to="profile" className="nav-link profile-link">
+            <FaRegUserCircle className="icon" size="1.5em" />
+            <span>Profile</span>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
