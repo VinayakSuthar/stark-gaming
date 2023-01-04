de78b39ccdd8c331451c1c8f2cf79d7798a3f2b1
vinayak <vinayak@gkmit.co>
2023-01-04 11:08:20 +0530

    Add sanitized data function

---
+++ src/utils/sanitizedData.js
@@ -0,0 +1,19 @@
+function sanitizeData(data) {
+  return data?.data?.data?.map((game) => {
+    const { Name, genres, background_image } = game.attributes;
+    const sanitizedGenres = genres?.data?.map((genre) => {
+      const { name, slug } = genre.attributes;
+      return { id: genre.id, name, slug };
+    });
+    return {
+      id: game.id,
+      name: Name,
+      genres: sanitizedGenres,
+      background_image: `${import.meta.env.VITE_IMAGE_URL}${
+        background_image?.data?.attributes?.url
+      }`,
+    };
+  });
+}
+
+export default sanitizeData;
