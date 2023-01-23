84fc8c0408318614233346a76c19f91c26dff9a0
vinayak <vinayak@gkmit.co>
2023-01-23 17:05:11 +0530

    Fix: useAxios config

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 function useAxios() {
   return axios.create({
-    baseURL: import.meta.env.VITE_URL,
+    baseURL: 'https://rawg-video-games-database.p.rapidapi.com/',
+    responseType: 'json',
+    params: { key: import.meta.env.VITE_RAWG_KEY },
+    headers: {
+      'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
+    },
   });
 }
 
