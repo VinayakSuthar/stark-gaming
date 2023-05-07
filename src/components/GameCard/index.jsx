1f87a458334e0aa3e5735ee4296066f1941f946c
Vinayak Suthar <sutharvinayak28@gmail.com>
2023-05-07 15:09:11 +0530

    tailwind game card

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -3,30 +3,30 @@ import { motion } from 'framer-motion';
 import { FiCheck } from 'react-icons/fi';
 import { MdDragIndicator } from 'react-icons/md';
 
-import './index.css';
-
 export default function GameCard({ data, isSelected, buttonValue, onStatusChange, onDragStart, draggable = false }) {
   const { id, background_image: backgroundImage, name, genres } = data;
   return (
     <div
-      className="game-card"
+      className="game-card relative max-sm:group-[.popular]:inline-block max-sm:group-[.popular]:w-72 max-sm:group-[.popular]:mx-3"
       draggable={draggable}
       onDragStart={() => onDragStart({ id, background_image: backgroundImage, name, genres })}
     >
-      {draggable && <MdDragIndicator className="drag-icon" />}
+      {draggable && <MdDragIndicator className="absolute top-0 right-0 text-xl bg-gray-950 rounded-tr-lg" />}
       <Link to={`/browse/game/${id}`}>
-        <img src={backgroundImage} alt="game" />
-        <h3 className="game-title" title={name}>
+        <img src={backgroundImage} alt="game" className="w-full aspect-video object-cover rounded-xl" />
+        <h3 className="game-title text-slate-200 truncate" title={name}>
           {name}
         </h3>
       </Link>
-      <p className="game-genre">{genres[0]?.name}</p>
+      <p className="game-genre font-medium">{genres[0]?.name}</p>
       <motion.button
-        className={`wishlist-btn ${!isSelected(id) ? '' : 'active-wishlist'}`}
+        className={`wishlist-btn bg-darkBlue text-lightBlue font-semibold text-base border-none outline-none py-1 px-2 rounded-md cursor-pointer mt-1 group ${
+          !isSelected(id) ? '' : 'active-wishlist'
+        }`}
         onClick={() => onStatusChange(id, name, genres, backgroundImage)}
         whileHover={{ scale: 1.15 }}
       >
-        <FiCheck />
+        <FiCheck className="hidden group-[.active-wishlist]:inline-block text-lightBlue align-middle text-[1.1rem] mr-1" />
         {buttonValue}
       </motion.button>
     </div>
