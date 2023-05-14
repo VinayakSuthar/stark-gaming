c429c559a440f1ce6f1cd1e1e4b536791f4efe5c
Vinayak Suthar <sutharvinayak28@gmail.com>
2023-05-14 17:16:20 +0530

    Tailwind search box

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -9,7 +9,6 @@ import useAxios from '../../hooks/useAxios';
 import useDebounce from '../../hooks/useDebounce';
 import useOutsideClick from '../../hooks/useOutsideClick';
 import loader from '../../assets/image/loader.svg';
-import './index.css';
 
 const backdrop = {
   hidden: { opacity: 0 },
@@ -49,33 +48,38 @@ function searchGame({ queryKey }) {
 
 function SearchResult({ searchValue, isLoading, isError, searchResult, onClose }) {
   if (searchValue.length === 0) {
-    return <p className="exception-block">Search Games</p>;
+    return <p className="mt-24 self-center text-xl">Search Games</p>;
   }
   if (isLoading) {
     return (
-      <div className="loader-container">
-        <img className="loader-svg" src={loader} alt="loader" />
+      <div className="mb-5 grid h-full place-items-center">
+        <img className="loader-svg w-2/5" src={loader} alt="loader" />
       </div>
     );
   }
   if (isError) {
     return (
-      <p className="exception-block">
+      <p className="mt-24 self-center text-xl">
         An error ocurred. <br />
         Please try again later
       </p>
     );
   }
   if (searchResult?.length === 0) {
-    return <p className="exception-block">No Result Found</p>;
+    return <p className="mt-24 self-center text-xl">No Result Found</p>;
   }
   if (searchResult?.length > 0) {
     return searchResult?.map(({ id, name, genres, background_image: backgroundImage }) => (
-      <Link to={`/browse/game/${id}`} key={id} onClick={onClose}>
-        <img src={backgroundImage} alt="title" className="result-image" />
-        <div className="result-content">
-          {name} <br />
-          <span className="search-genre">{genres[0]?.name || 'No data'}</span>
+      <Link
+        to={`/browse/game/${id}`}
+        key={id}
+        onClick={onClose}
+        className="flex max-w-full gap-x-3 text-xl text-lightBlue"
+      >
+        <img src={backgroundImage} alt="title" className="aspect-video w-24 rounded-md object-cover" />
+        <div className="result-content w-4/5">
+          <h4 className="w-4/5 truncate">{name}</h4>
+          <span className="text-base text-secondary">{genres[0]?.name || 'No data'}</span>
         </div>
       </Link>
     ));
@@ -113,17 +117,22 @@ export default function SearchBox({ onClose }) {
   return (
     <motion.div
       key="backdrop"
-      className="search-box-container"
+      className="fixed inset-0 z-20 grid h-screen place-items-center bg-black/70 backdrop-blur-sm"
       variants={backdrop}
       initial="hidden"
       animate="visible"
       exit="exit"
     >
-      <motion.div key="modal" className="search-box" ref={searchRef} variants={dropIn}>
-        <div className="search-bar">
-          <BiSearch />
+      <motion.div
+        key="modal"
+        className="relative flex h-[350px] w-[clamp(35%,500px,90%)] flex-col rounded-xl border-2 border-primary/70 bg-veryDarkBlue sm:h-[450px]"
+        ref={searchRef}
+        variants={dropIn}
+      >
+        <div className="flex items-center gap-x-2 border-b-2 border-primary/70 px-4">
+          <BiSearch size={24} />
           <input
-            className="search-input"
+            className="border-none bg-transparent px-2 py-5 text-xl text-lightBlue outline-none"
             type="text"
             placeholder="Search..."
             value={searchValue}
@@ -132,7 +141,7 @@ export default function SearchBox({ onClose }) {
             onChange={(e) => setSearchValue(e.target.value)}
           />
         </div>
-        <div className="search-result">
+        <div className="flex h-full flex-col gap-y-4 overflow-x-auto px-4 py-4">
           <SearchResult
             searchValue={searchValue}
             searchResult={searchResult}
